f364da93956f67dcce641eeb1f547890650d6553
lin.peng <lin.peng@all-union.com.cn>
2017-05-10 09:55:56 +0800

    20170510

--- sketch_may08a/sketch_may08a.ino
+++ sketch_may08a/sketch_may08a.ino
@@ -1,5 +1,6 @@
 const int pin_pul = 8;
 const int pin_dir = 7;
+const int pin_ctrl = 6;
 const int indexPin = 13;
 const int tonePin = 12;
 const int relay_A = 4;
@@ -8,24 +9,25 @@ const int emptiedPin = 3;
 char command;
 int emptiedStatus = 0;
 
-void run(int pin_pul, int dur) {
+void run(int dur) {
+  digitalWrite(pin_ctrl, LOW);
   for (int i = 0; i < dur; ++i) {
     digitalWrite(pin_pul, HIGH);
     delayMicroseconds(500);
     digitalWrite(pin_pul, LOW);
     delayMicroseconds(500);
   }
+  digitalWrite(pin_ctrl, HIGH);
 }
 
 void rightRun(int dur) {
-
   digitalWrite(pin_dir, HIGH);
-  run(pin_pul, dur);
+  run(dur);
 }
 
 void leftRun(int dur) {
   digitalWrite(pin_dir, LOW);
-  run(pin_pul, dur);
+  run(dur);
 }
 
 void setup() {
@@ -36,6 +38,7 @@ void setup() {
   pinMode(relay_B, OUTPUT);
   pinMode(pin_pul, OUTPUT);
   pinMode(pin_dir, OUTPUT);
+  pinMode(pin_ctrl, OUTPUT);
   Serial.begin(9600, SERIAL_8N1);
   while (!Serial) {
     ;
@@ -56,53 +59,55 @@ void loop() {
 
 
   if ('h' == command && emptiedStatus == 0) {
-    //0
+    //1、蜂鸣器长鸣一次
     tone(tonePin, 520);
     delay(1000);
     noTone(tonePin);
 
-    //1、指示灯亮
+    //2、指示灯亮
     digitalWrite(indexPin, HIGH);
 
-    //2、电机反向转动4周，400转动一周
+    //3、电机反向转动4周，400转动一周
     rightRun(400 * 4);
 
     Serial.write('success');
 
-    //3、继电器A组松
+    //4、继电器A组松
     digitalWrite(relay_A, HIGH);
     delay(1000);
 
-    //4、继电器A组紧
+    //5、继电器A组紧
     digitalWrite(relay_A, LOW);
     delay(500);
 
-    //5、继电器B组松
+    //6、继电器B组松
     digitalWrite(relay_B, HIGH);
     delay(500);
 
-    //6、电机正向转动4周，400转动一周
+    //7、电机正向转动4周，400转动一周
     leftRun(400 * 4);
 
-    //7、继电器B组紧
+    //8、继电器B组紧
     digitalWrite(relay_B, LOW);
     delay(500);
 
-    //8、电机正向转动6周，400转动一周
+    //9、电机正向转动6周，400转动一周
     rightRun(400 * 8);
 
-    //9、电机反向转动5周，400转动一周
+    //10、电机反向转动5周，400转动一周
     leftRun(400 * 8);
 
-    //10、指示灯灭
-    tone(tonePin, 520);
-    delay(500);
-    noTone(tonePin);
-    delay(500);
-    tone(tonePin, 520);
-    delay(500);
-    noTone(tonePin);
+    //11、指示灯灭
     digitalWrite(indexPin, LOW);
+
+    //12、蜂鸣器短鸣两次
+    tone(tonePin, 520);
+    delay(500);
+    noTone(tonePin);
+    delay(500);
+    tone(tonePin, 520);
+    delay(500);
+    noTone(tonePin);
   }
 
   if (emptiedStatus == 1) {
